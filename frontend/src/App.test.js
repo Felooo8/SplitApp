4e86c68db4a2c402339d866dd4398a54c2a8db15
felo <felobanka@gmail.com>
2022-12-29 16:47:56 +0100

    Init tests for Sign In Page

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders sign in page - not logged in", () => {
+  render(<App />);
+  screen.debug();
+  const typography = screen.getByText("Sign in");
+  expect(typography).toBeInTheDocument();
+
+  const usernameField = screen.getByRole("textbox", { name: /username/i });
+  expect(usernameField).toBeInTheDocument();
+  expect(usernameField).toHaveValue("");
+  const passwordField = screen.getByLabelText(/Password/i);
+  expect(passwordField).toBeInTheDocument();
+  expect(usernameField).toHaveValue("");
+
+  let setUsername = "Some username";
+  fireEvent.change(usernameField, {
+    target: { value: setUsername },
+  });
+  expect(usernameField).toHaveValue(setUsername);
+
+  let setPassword = "Some passowrd";
+  fireEvent.change(passwordField, {
+    target: { value: setPassword },
+  });
+  expect(passwordField).toHaveValue(setPassword);
+
+  //   const typographyBad = screen.queryByText("Not in document");
+  //   expect(typographyBad).toBeNull();
+});
